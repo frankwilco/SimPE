3aad123879ef2aff2e524b4bf4dfb6b1eaa581eb
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2005-05-09 22:18:09 +0000

    Add "new" to FileName to eliminate some warnings

--- trunk/fullsimpe/SimPe BHAV/TtabWrapper.cs
+++ trunk/fullsimpe/SimPe BHAV/TtabWrapper.cs
@@ -62,7 +62,7 @@ namespace SimPe.PackedFiles.Wrapper
 		/// <summary>
 		/// Returns the Filename
 		/// </summary>
-		public string FileName 
+		public new string FileName 
 		{
 			get { return Helper.ToString(filename); }
 		}
