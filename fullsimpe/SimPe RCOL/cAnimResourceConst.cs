0b4ff1efd24ba3c84edb9fe7f3b5d1e4b34b0a14
ambertation <ambertation@b0317113-201e-0410-9332-a427c079c53a>
2005-05-17 15:33:37 +0000

    Changes by Emily:     + Added some Code to process cAnimResourceConst Blocks in RCOL Files

--- fullsimpe/SimPe RCOL/cAnimResourceConst.cs
+++ fullsimpe/SimPe RCOL/cAnimResourceConst.cs
@@ -21,39 +21,19 @@ using System;
 
 namespace SimPe.Plugin
 {
-	public class AnimBlock1 
+	public class AnimBlock 
 	{
-		uint[] datai;
-		short[] datas;
-		string name;
-
-		internal AnimBlock1() 
+		protected string name;
+		public string Name 
 		{
-			datai = new uint[5];
-			datas = new short[4];
+			get { return name; }
+			set { name = value; }
+		}
+
+		internal AnimBlock() 
+		{
 			name = "";
-		}
-
-		/// <summary>
-		/// Unserializes a BinaryStream into the Attributes of this Instance
-		/// </summary>
-		/// <param name="reader">The Stream that contains the FileData</param>
-		internal int UnserializeData(System.IO.BinaryReader reader)
-		{
-			datai[0] = reader.ReadUInt32();
-			datai[1] = reader.ReadUInt32();
-
-			datas[0] = reader.ReadInt16();
-			datas[1] = reader.ReadInt16();
-			datas[2] = reader.ReadInt16();
-			datas[3] = reader.ReadInt16();
-
-			datai[2] = reader.ReadUInt32();
-			datai[3] = reader.ReadUInt32();
-			datai[4] = reader.ReadUInt32();
-
-			return datas[1];
-		}
+		}		
 
 		/// <summary>
 		/// Unserializes a BinaryStream into the Attributes of this Instance
@@ -69,8 +49,191 @@ namespace SimPe.Plugin
 				name += ch;
 			}
 
-			return name.Length;
+			return name.Length+1;
 		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+
+	}
+
+	public class AnimBlock1 : AnimBlock
+	{
+		uint[] datai;		
+		short[] datas;
+		#region Attributes
+		public uint Unknown1 
+		{
+			get { return datai[0]; }
+			set { datai[0] = value; }
+		}
+		public uint Unknown2 
+		{
+			get { return datai[1]; }
+			set { datai[1] = value; }
+		}
+		public uint Unknown3 
+		{
+			get { return datai[2]; }
+			set { datai[2] = value; }
+		}
+		public uint Unknown4 
+		{
+			get { return datai[3]; }
+			set { datai[3] = value; }
+		}
+		public uint Unknown5 
+		{
+			get { return datai[4]; }
+			set { datai[4] = value; }
+		}
+		public short SUnknown1 
+		{
+			get { return datas[0]; }
+			set { datas[0] = value; }
+		}
+		public short SUnknown2 
+		{
+			get { return datas[1]; }
+			set { datas[1] = value; }
+		}
+		public short SUnknown3 
+		{
+			get { return datas[2]; }
+			set { datas[2] = value; }
+		}
+		public short SUnknown4 
+		{
+			get { return datas[3]; }
+			set { datas[3] = value; }
+		}
+		#endregion
+
+		internal AnimBlock1() 
+		{
+			datai = new uint[6];
+			datas = new short[4];
+		}
+
+		/// <summary>
+		/// Unserializes a BinaryStream into the Attributes of this Instance
+		/// </summary>
+		/// <param name="reader">The Stream that contains the FileData</param>
+		internal int UnserializeData(System.IO.BinaryReader reader)
+		{
+			datai[0] = reader.ReadUInt32();
+			datai[1] = reader.ReadUInt32();
+
+			datas[0] = reader.ReadInt16();
+			datas[1] = reader.ReadInt16();
+			datas[2] = reader.ReadInt16();
+			datas[3] = reader.ReadInt16();			
+
+			datai[2] = reader.ReadUInt32();
+			datai[3] = reader.ReadUInt32();
+			datai[4] = reader.ReadUInt32();
+
+			return datas[1];
+		}		
+	}
+
+	public class AnimBlock2 : AnimBlock
+	{
+		uint[] datai;
+		#region Attributes
+		public uint Unknown1 
+		{
+			get { return datai[0]; }
+			set { datai[0] = value; }
+		}
+		public uint Unknown2 
+		{
+			get { return datai[1]; }
+			set { datai[1] = value; }
+		}
+		public uint Unknown3 
+		{
+			get { return datai[2]; }
+			set { datai[2] = value; }
+		}
+		public uint Unknown4 
+		{
+			get { return datai[3]; }
+			set { datai[3] = value; }
+		}
+		public uint Unknown5 
+		{
+			get { return datai[4]; }
+			set { datai[4] = value; }
+		}
+		public uint Unknown6 
+		{
+			get { return datai[5]; }
+			set { datai[5] = value; }
+		}
+		#endregion
+
+		internal AnimBlock2() 
+		{
+			datai = new uint[6];			
+		}
+
+		/// <summary>
+		/// Unserializes a BinaryStream into the Attributes of this Instance
+		/// </summary>
+		/// <param name="reader">The Stream that contains the FileData</param>
+		internal int UnserializeData(System.IO.BinaryReader reader)
+		{
+			datai[0] = reader.ReadUInt32();
+			datai[1] = reader.ReadUInt32();			
+			datai[2] = reader.ReadUInt32();
+			datai[3] = reader.ReadUInt32();
+			datai[4] = reader.ReadUInt32();
+			datai[5] = reader.ReadUInt32();
+
+			//returning highest 3-Bits xxx00000000000000000000000000000
+			return ((int)datai[4] >> 0x1D) & 0x7;
+		}		
+	}
+
+	public class AnimBlock3
+	{
+		uint[] datai;
+		#region Attributes
+		public uint Unknown1 
+		{
+			get { return datai[0]; }
+			set { datai[0] = value; }
+		}
+		public uint Unknown2 
+		{
+			get { return datai[1]; }
+			set { datai[1] = value; }
+		}		
+		#endregion
+
+		internal AnimBlock3() 
+		{
+			datai = new uint[2];			
+		}
+
+		/// <summary>
+		/// Unserializes a BinaryStream into the Attributes of this Instance
+		/// </summary>
+		/// <param name="reader">The Stream that contains the FileData</param>
+		internal void UnserializeData(System.IO.BinaryReader reader)
+		{
+			datai[0] = reader.ReadUInt32();
+			datai[1] = reader.ReadUInt32();			
+		}	
+	
+		public override string ToString()
+		{
+			return Helper.HexString(datai[0]);
+		}
+
 	}
 
 	/// <summary>
@@ -97,6 +260,8 @@ namespace SimPe.Plugin
 		string objmod;
 
 		AnimBlock1[] ab1;
+		AnimBlock2[] ab2;
+		AnimBlock3[] ab3;
 		#endregion
 		
 
@@ -117,6 +282,8 @@ namespace SimPe.Plugin
 			objmod = "";
 
 			ab1 = new AnimBlock1[0];
+			ab2 = new AnimBlock2[0];
+			ab3 = new AnimBlock3[0];
 		}
 		
 		#region IRcolBlock Member
@@ -144,7 +311,17 @@ namespace SimPe.Plugin
 
 		static void AlignReader(System.IO.BinaryReader reader, int ct)
 		{
-			int add = (ct%4);
+			int add = 0;
+			if (ct%2==0) //even
+			{
+				add = (ct%4);
+			} 
+			else //uneven
+			{
+				add = ct%2;
+				if (((add+ct)%4)==0) add += 2;
+			}
+			
 			while (add-->0) reader.ReadByte();
 		}
 
@@ -159,11 +336,12 @@ namespace SimPe.Plugin
 			for (int i=0;i<headerf.Length; i++) headerf[i] = reader.ReadUInt32();
 			
 			objname = Helper.ToString(reader.ReadBytes(headerb[5]));
+			reader.ReadByte(); //read the terminating 0
 			objmod = Helper.ToString(reader.ReadBytes(headerb[0]));
-
+			reader.ReadByte(); //read the terminating 0
 			//align reader
 			int ct = headerb[0] + headerb[5];
-			AlignReader(reader, ct);
+			AlignReader(reader, ct+2);
 			
 			//--- part1 ---
 			ab1 = new AnimBlock1[ct1];
@@ -175,7 +353,30 @@ namespace SimPe.Plugin
 				ct3 += ab1[i].UnserializeData(reader);
 			}
 			for (int i=0; i<ab1.Length; i++) len += ab1[i].UnserializeName(reader);
+			ab1[0].Unknown1 = (uint)len;
+			ab1[0].Unknown2 = (uint)(len%4);
+			ab1[0].Unknown3 = (uint)(len%2);
 			AlignReader(reader, len);
+
+			//--- part2 ---
+			ab2 = new AnimBlock2[ct3];
+			len = 0;
+			int ct4 = 0;
+			for (int i=0; i<ab2.Length; i++) 
+			{
+				ab2[i] = new AnimBlock2();
+				ct4 += ab2[i].UnserializeData(reader);
+			}
+			for (int i=0; i<ab2.Length; i++) len += ab2[i].UnserializeName(reader);
+			AlignReader(reader, len);
+
+			//--- part3 ---
+			ab3 = new AnimBlock3[ct4];
+			for (int i=0; i<ab3.Length; i++) 
+			{
+				ab3[i] = new AnimBlock3();
+				ab3[i].UnserializeData(reader);
+			}
 		}
 
 		/// <summary>
@@ -216,7 +417,18 @@ namespace SimPe.Plugin
 		{
 			if (form==null) form = new fShapeRefNode(); 
 			form.tb_arc_ver.Text = "0x"+Helper.HexString(this.version);
+#if DEBUG
+			form.tb_arc_data.Text = "---Debugging Session, do not Save!!!---";
+#else
 			form.tb_arc_data.Text = Helper.BytesToHexList(this.data);
+#endif
+
+			form.tb_arc_lb1.Items.Clear();
+			form.tb_arc_lb2.Items.Clear();
+
+			foreach (AnimBlock1 ab in this.ab1) form.tb_arc_lb1.Items.Add(ab);
+			foreach (AnimBlock2 ab in this.ab2) form.tb_arc_lb2.Items.Add(ab);
+			foreach (AnimBlock3 ab in this.ab3) form.tb_arc_lb3.Items.Add(ab);
 		}
 	}
 }
