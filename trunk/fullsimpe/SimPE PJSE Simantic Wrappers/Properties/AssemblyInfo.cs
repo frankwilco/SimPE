ca55465cd81464da07d1d098fe1c0c378f07be82
ambertation <ambertation@b0317113-201e-0410-9332-a427c079c53a>
2006-11-26 12:48:29 +0000

    Added SimPE PJSE Simantic Wrappers Project

---
+++ trunk/fullsimpe/SimPE PJSE Simantic Wrappers/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimPE PJSE Simantic Wrappers")]
+[assembly: AssemblyDescription("Contains Resource Wrappers for SimAntic Resources")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Maintained by PLJones")]
+[assembly: AssemblyProduct("SimPE PJSE Simantic Wrappers")]
+[assembly: AssemblyCopyright("Copyright © PLJones 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0b7dd9f-948f-4a38-8859-df55de85ffc7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
