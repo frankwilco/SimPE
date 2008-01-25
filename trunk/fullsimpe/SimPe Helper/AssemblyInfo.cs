99c3b740cbc923a5cc9e91cbd6cab2f2d2519480
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-01-25 21:00:26 +0000

    Already gave 0_65_8 to Inge.  Oops.

--- trunk/fullsimpe/SimPe Helper/AssemblyInfo.cs
+++ trunk/fullsimpe/SimPe Helper/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte oder die standardmäßige Revision und Buildnummer 
 // mit '*' angeben:
 
-[assembly: AssemblyVersion("0.65.7.*")]
+[assembly: AssemblyVersion("0.65.8.*")]
 
 //
 // Um die Assembly zu signieren, müssen Sie einen Schlüssel angeben. Weitere Informationen 
