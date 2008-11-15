692433b40c5b8e75ec013408e96ebd9762095df0
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-11-15 14:54:54 +0000

    Um. Released to Inge.  As was 15.  Some time ago.

--- trunk/fullsimpe/SimPe Helper/AssemblyInfo.cs
+++ trunk/fullsimpe/SimPe Helper/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte oder die standardmäßige Revision und Buildnummer 
 // mit '*' angeben:
 
-[assembly: AssemblyVersion("0.73.14.*")]
+[assembly: AssemblyVersion("0.73.16.*")]
 
 //
 // Um die Assembly zu signieren, müssen Sie einen Schlüssel angeben. Weitere Informationen 
