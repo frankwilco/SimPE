37645d366058a9dc51d53f1cc45189d2fe22e44a
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-11-20 14:41:17 +0000

    Released at 08:45 to Inge

--- fullsimpe/SimPe Main/AssemblyInfo.cs
+++ fullsimpe/SimPe Main/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte oder die standardmäßige Revision und Buildnummer 
 // mit '*' angeben:
 
-[assembly: AssemblyVersion("0.73.19.*")]
+[assembly: AssemblyVersion("0.73.20.*")]
 
 //
 // Um die Assembly zu signieren, müssen Sie einen Schlüssel angeben. Weitere Informationen 
